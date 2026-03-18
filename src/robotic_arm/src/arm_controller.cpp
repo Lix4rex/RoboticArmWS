cf6d54704107abeee2da2e9fc6a1f7b4d1bfb46d
Lix4rex <axelparis6@gmail.com>
2026-03-18 14:36:36 +0100

    Ajout de log pour le calcul de position 3D à partir des angles

--- src/robotic_arm/src/arm_controller.cpp
+++ src/robotic_arm/src/arm_controller.cpp
@@ -1,6 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64_multi_array.hpp"
 #include "robot_msgs/msg/target_position.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "cmath"
 
 using namespace std::chrono_literals;
 
@@ -18,11 +20,41 @@ class ArmController : public rclcpp::Node{
                                 std::bind(&ArmController::target_position_callback, this, std::placeholders::_1)
                         );
 
+                        position_sub_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+                                "/position_controller/commands", 10,
+                                std::bind(&ArmController::position_controller_callback, this, std::placeholders::_1)
+                        );
+
                         RCLCPP_INFO(this->get_logger(), "Launched without error");
                 }
 
 
         private:
+
+                void compute_3D_position(double theta_1, double theta_2, double theta_3, double theta_4){
+                        
+                        double x = 0.0;
+                        double y = 0.0;
+                        double z = 0.0;
+
+                        z = ARM_HEIGHT*(sin(theta_2) + sin(theta_3) + sin(theta_4));
+
+                        double value = ARM_HEIGHT*(cos(theta_2) + cos(theta_3) + cos(theta_4));
+                        x = value*cos(theta_1);
+                        y = value*sin(theta_1);
+
+                        RCLCPP_INFO(this->get_logger(), "x=%f, y=%f, z=%f", x, y, z);
+                        
+                }
+
+                void position_controller_callback(const std_msgs::msg::Float64MultiArray::SharedPtr position_msg){
+                        double first_arm_joint_position  = position_msg->data[0];
+                        double second_arm_joint_position = position_msg->data[1];
+                        double third_arm_joint_position  = position_msg->data[2];
+                        double fourth_arm_joint_position = position_msg->data[3];
+
+                        compute_3D_position(first_arm_joint_position, second_arm_joint_position, third_arm_joint_position, fourth_arm_joint_position);
+                }
 
                 void target_position_callback(const robot_msgs::msg::TargetPosition::SharedPtr target_msg){
                         double x_target = target_msg->x_target;
@@ -34,8 +66,10 @@ class ArmController : public rclcpp::Node{
                 }
 
                 rclcpp::Subscription<robot_msgs::msg::TargetPosition>::SharedPtr target_position_sub_;
-
+                rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr position_sub_;
                 rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr position_pub_;
+
+                double ARM_HEIGHT = 3.0;
 };
 
 
